7e112868f6440650351704e16a8cdb4bb3143214
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2024-08-01 14:12:49 +0300

    [WIP]: rpi5: increase number of mmap regions and xlat tables
    Change-Id: Ibe3a22965f5178d73608e946db46020bc1046c62
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- plat/rpi/rpi5/include/platform_def.h
+++ plat/rpi/rpi5/include/platform_def.h
@@ -117,8 +117,8 @@
 #define PLAT_PHY_ADDR_SPACE_SIZE	(ULL(1) << 40)
 #define PLAT_VIRT_ADDR_SPACE_SIZE	(ULL(1) << 40)
 
-#define MAX_MMAP_REGIONS		8
-#define MAX_XLAT_TABLES			5
+#define MAX_MMAP_REGIONS		16
+#define MAX_XLAT_TABLES			16
 
 #define MAX_IO_DEVICES			U(3)
 #define MAX_IO_HANDLES			U(4)
